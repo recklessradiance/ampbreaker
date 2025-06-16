e8c8723a898e8c3194736977f2bdf4db7922db2b
RC Reddy <rcreddyn@amazon.com>
2025-06-17 00:17:12 +0530

    Initial setup

---
+++ src/main/java/com/rockstarpainkiller/ampbreaker/AmpbreakerApplication.java
@@ -0,0 +1,13 @@
+package com.rockstarpainkiller.ampbreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmpbreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmpbreakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rockstarpainkiller/ampbreaker/AmpbreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rockstarpainkiller.ampbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmpbreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
